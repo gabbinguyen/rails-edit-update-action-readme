77df2d4ebcf328de145a51780452cb566f34dbf2
Gabbi Nguyen <gabbinguyen@gmail.com>
2020-11-01 19:47:19 -0600

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+@article = Article.create(title: "My Article", description: "My article desc")
